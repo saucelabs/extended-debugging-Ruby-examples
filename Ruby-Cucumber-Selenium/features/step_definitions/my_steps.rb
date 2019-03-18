96250da828fc77f6519e15f4953e22e305ae5b2e
Sean Enright <33846906+seanenright@users.noreply.github.com>
2019-03-18 13:51:46 -0700

    [perf-562] update code samples (#5)

--- Ruby-Cucumber-Selenium/features/step_definitions/my_steps.rb
+++ Ruby-Cucumber-Selenium/features/step_definitions/my_steps.rb
@@ -3,24 +3,24 @@ Before do |scenario|
 end
 
 Given(/^I am on the Login Page$/) do
-  @driver.get 'http://www.saucedemo.com'
+  @driver.get "http://www.saucedemo.com"
 end
 
 When(/^I log in with valid credentials$/) do
-  username = ENV['PERF_USERNAME'] ||= 'standard_user'
+  username = ENV["PERF_USERNAME"] ||= "standard_user"
   @driver.find_element(css: "[data-test=username]").send_keys username
-  @driver.find_element(css: "[data-test=password]").send_keys 'secret_sauce'
+  @driver.find_element(css: "[data-test=password]").send_keys "secret_sauce"
   @driver.find_element(css: "[type=submit]").click
 end
 
 When(/^I log in as a performance regression user$/) do
-  @driver.find_element(css: "[data-test=username]").send_keys'performance_glitch_user'
-  @driver.find_element(css: "[data-test=password]").send_keys 'secret_sauce'
+  @driver.find_element(css: "[data-test=username]").send_keys"performance_glitch_user"
+  @driver.find_element(css: "[data-test=password]").send_keys "secret_sauce"
   @driver.find_element(css: "[type=submit]").click
 end
 
 Then(/^I should be logged in$/) do
-  expect(@driver.current_url).to eq 'https://www.saucedemo.com/inventory.html'
+  expect(@driver.current_url).to eq "https://www.saucedemo.com/inventory.html"
 end
 
 Given(/^I am on the Product Page$/) do
@@ -38,10 +38,18 @@ end
 
 Then(/^I assert the sauce:performance custom command identifies page load regressions/) do
   performance = @driver.execute_script("sauce:performance", {"name":@feature_name, "metrics": ["load"] })
-  expect(performance["result"] == "pass").to be true
+  # The custom command will return "pass" if the test falls within the predicted baseline
+  # or "fail"  if the performance metric falls outside the predicted baseline.
+  # customers can decide how strict they want to be in failing tests by setting thier own
+  # failure points.
+  performance["result"] != "pass" ?
+    (expect(performance["details"]["load"] < 5000).to be true) :
+    (expect(performance["result"] == "pass").to be true)
 end
 
 Then(/^I assert the sauce:performance custom command identifies speedIndex regressions/) do
-  performance = @driver.execute_script("sauce:performance", {"name":@feature_name, "metrics": ["speedIndex"] })
-  expect(performance["result"] == "pass").to be true
+  performance = @driver.execute_script("sauce:performance", {"name":@feature_name, "metrics": ["timeToFirstInteractive"] })
+  performance["result"] != "pass" ?
+    (expect(performance["details"]["timeToFirstInteractive"] < 5000).to be true) :
+    (expect(performance["result"] == "pass").to be true)
 end
--- Ruby-Cucumber-Watir/features/step_definitions/my_steps.rb
+++ Ruby-Cucumber-Watir/features/step_definitions/my_steps.rb
@@ -3,28 +3,28 @@ Before do |scenario|
 end
 
 Given(/^I am on the Login Page$/) do
-  @browser.goto 'www.saucedemo.com'
+  @browser.goto "www.saucedemo.com"
 end
 
 Given(/^I am on the Product Page$/) do
-  @browser.goto 'www.saucedemo.com/inventory.html'
+  @browser.goto "www.saucedemo.com/inventory.html"
 end
 
 When(/^I log in with valid credentials$/) do
-  username = ENV['PERF_USERNAME'] ||= 'standard_user'
-  @browser.text_field(data_test: 'username').set username
-  @browser.text_field(data_test: 'password').set 'secret_sauce'
-  @browser.button(type: 'submit').click
+  username = ENV["PERF_USERNAME"] ||= "standard_user"
+  @browser.text_field(data_test: "username").set username
+  @browser.text_field(data_test: "password").set "secret_sauce"
+  @browser.button(type: "submit").click
 end
 
 When(/^I log in as a performance regression user$/) do
-  @browser.text_field(data_test: 'username').set 'performance_glitch_user'
-  @browser.text_field(data_test: 'password').set 'secret_sauce'
-  @browser.button(type: 'submit').click
+  @browser.text_field(data_test: "username").set "performance_glitch_user"
+  @browser.text_field(data_test: "password").set "secret_sauce"
+  @browser.button(type: "submit").click
 end
 
 Then(/^I should be logged in$/) do
-  expect(@browser.url).to eq 'https://www.saucedemo.com/inventory.html'
+  expect(@browser.url).to eq "https://www.saucedemo.com/inventory.html"
 end
 
 Then(/^I check for sauce:performanceLogs/) do
@@ -38,10 +38,18 @@ end
 
 Then(/^I assert the sauce:performance custom command identifies page load regressions/) do
   performance = @browser.execute_script("sauce:performance", {"name":@feature_name, "metrics": ["load"] })
-  expect(performance['result'] == "pass").to be true
+  # The custom command will return "pass" if the test falls within the predicted baseline
+  # or "fail"  if the performance metric falls outside the predicted baseline.
+  # customers can decide how strict they want to be in failing tests by setting thier own
+  # failure points.
+  performance["result"] != "pass" ? 
+    (expect(performance["details"]["load"]["actual"] < 5000).to be true) :
+    (expect(performance["result"] == "pass").to be true)
 end
 
 Then(/^I assert the sauce:performance custom command identifies speedIndex regressions/) do
-  performance = @browser.execute_script("sauce:performance", {"name":@feature_name, "metrics": ["speedIndex"] })
-  expect(performance['result'] == "pass").to be true
+  performance = @browser.execute_script("sauce:performance", {"name":@feature_name, "metrics": ["timeToFirstInteractive"] })
+  performance["result"] != "pass" ? 
+    (expect(performance["details"]["timeToFirstInteractive"]["actual"] < 5000).to be true) :
+    (expect(performance["result"] == "pass").to be true)
 end
